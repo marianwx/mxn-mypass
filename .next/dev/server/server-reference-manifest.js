9580bc4da64a0bb7c9e51f2969583fe2dcba66d7
.maran <maransmk69@gmail.com>
2025-11-10 07:18:19 +0200

    .

--- .next/dev/server/server-reference-manifest.js
+++
@@ -1,1 +0,0 @@
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"qYwkL8NUEu/1uElsxf+GSMhAqy94O81BW3Iwr+tcQD0=\"\n}"
---
+++ .nuxt/dist/server/server.mjs
@@ -0,0 +1,1 @@
+export { default } from "file:///Users/maryan/Documents/GitHub/mxn-mypass/node_modules/@nuxt/vite-builder/dist/runtime/vite-node.mjs"
